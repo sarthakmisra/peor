b6127d506b8bb0ecb415e9132b4e3216172ce610
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2022-02-18 15:32:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function view() {
+  document.getElementById("story").innerHTML = "Everythin' was fine . Guests arrived into the house grandpa asked grandma to get tea for the guests and she did so . A few minutes later she went to the living room and asked grandpa to have a word , she said that her chest was paining . Grandpa called the docter but before the docter arrived she left the world 😭😭😭😭😭😭😭.";
+ }
+function viewphotos() {
+     document.getElementById("abcd").innerHTML="<img src='pic1.JPG' id='xyz_image' style='height:800px; width:800px'>";
+     
+}
+function hidephotos() {
+    document.getElementById("abcd").innerHTML="";
+}
